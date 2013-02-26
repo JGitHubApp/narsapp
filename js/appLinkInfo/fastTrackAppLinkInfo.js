8988136250d483f99d330e86002e45fe1cdeaf49
studenofnars <studentofnars@gmail.com>
2013-02-26 10:41:12 -0600

    Added a few images

--- js/appLinkInfo/fastTrackAppLinkInfo.js
+++ js/appLinkInfo/fastTrackAppLinkInfo.js
@@ -1,9 +1,9 @@
 var fastTrackAppLinkInfo = new AppLinkLog(
   [
     ['Back', '../images/glossy-black-icon-arrows-arrow-styled-left.png', '../index.html', '#CCCCCC'],
-    ['Conductor', '../images/glossy-black-icon-transport-travel-transportation-train4.png', '#', '#FFFFFF'],
-    ['Freight Car', '../images/glossy-black-icon-transport-travel-transportation-train6-sc36.png', '#', '#FFFFFF'],
-    ['Signal Systems', '../images/filler.png', '../htdocs/signalSystems.html', '#FFFFFF']
+    ['Conductor', '../images/glossy-black-icon-transport-travel-transportation-train4.png', '#', '#0000EE'],
+    ['Freight Car', '../images/glossy-black-icon-transport-travel-transportation-train6-sc36.png', '#', '#EE0000'],
+    ['Signal Systems', '../images/glossy-black-icon-signs-stop-lights2.png', '../htdocs/signalSystems.html', '#EEEE00']
   ],
 
   [
--- js/appLinkInfo/forMeAppLinkInfo.js
+++ js/appLinkInfo/forMeAppLinkInfo.js
@@ -2,7 +2,7 @@ var forMeAppLinkInfo = new AppLinkLog(
   [
     ['Back', '../images/glossy-black-icon-arrows-arrow-styled-left.png', '../index.html', '#CCCCCC'],
     ['Typical Mandatory Industry Requirements', '../images/filler.png', '#', 'white'],
-    ['Successful Railroad Worker Attributes', '../images/filler.png', '#', 'white']
+    ['Successful Railroad Worker Attributes', '../images/manOnRailroadTracks.png', '#', '#33CC33']
   ],
 
   [
