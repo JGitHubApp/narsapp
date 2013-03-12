ffef8e0d76f3f7c0a694a44a1dfdc0916868babd
jason_gitaccount <forgitthisone@gmail.com>
2013-03-12 05:27:44 -0500

    link correction

--- js/appLinkInfo/railroadIndustryAppLinkInfo.js
+++ js/appLinkInfo/railroadIndustryAppLinkInfo.js
@@ -1,7 +1,7 @@
 var railroadIndustryAppLinkInfo = new AppLinkLog(
   [
     ['Back', '../images/glossy-black-icon-arrows-arrow-styled-left.png', '../index.html', '#CCCCCC'],
-    ['ASLRRA Homepage', '../images/filler.png', 'http://www.aslrra.com', 'white'],
+    ['ASLRRA Homepage', '../images/filler.png', 'http://www.aslrra.org', 'white'],
     ['Class I Railroads', '../images/glossy-black-icon-transport-travel-transportation-train8-sc43.png', '../htdocs/classOneRailroads.html', '#FF4400']
   ],
 
