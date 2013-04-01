7941382965e62a9d98149cfe7d2069ff17462a84
jason_gitaccount <forgitthisone@gmail.com>
2013-04-01 15:33:45 -0500

    Added conductor page

--- js/appLinkInfo/fastTrackAppLinkInfo.js
+++ js/appLinkInfo/fastTrackAppLinkInfo.js
@@ -1,7 +1,7 @@
 var fastTrackAppLinkInfo = new AppLinkLog(
   [
     ['Back', '../images/icons/glossy-black-icon-arrows-arrow-styled-left.png', '../index.html', '#CCC'],
-    ['Conductor', '../images/icons/glossy-black-icon-transport-travel-transportation-train4.png', '#', '#00E'],
+    ['Conductor', '../images/icons/glossy-black-icon-transport-travel-transportation-train4.png', '../htdocs/conductor.html', '#00E'],
     ['Freight Car', '../images/icons/glossy-black-icon-transport-travel-transportation-train6-sc36.png', '#', '#E00'],
     ['Signal Systems', '../images/icons/glossy-black-icon-signs-stop-lights2.png', '../htdocs/signalSystems.html', '#EE0']
   ],
@@ -15,13 +15,14 @@ var fastTrackAppLinkInfo = new AppLinkLog(
 
   [
     ,
-    ,
+    conductorAppLinkInfo,
     ,
     signalSystemsAppLinkInfo
   ]
 );
 
-delete signalSystemsAppLinkInfo
+delete conductorAppLinkInfo;
+delete signalSystemsAppLinkInfo;
 
 function loadFastTrackAppLinks() {
   loadAppLinks(fastTrackAppLinkInfo, document.getElementById('container'));
