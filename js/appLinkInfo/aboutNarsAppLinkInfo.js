1f9253e0757f84177c19bdbe7de3ee159edef83f
jason_gitaccount <forgitthisone@gmail.com>
2013-04-01 00:28:44 -0500

    Added picture gallery

--- js/appLinkInfo/aboutNarsAppLinkInfo.js
+++ js/appLinkInfo/aboutNarsAppLinkInfo.js
@@ -3,7 +3,7 @@ var aboutNarsAppLinkInfo = new AppLinkLog(
     ['Back', '../images/icons/glossy-black-icon-arrows-arrow-styled-left.png', '../index.html', '#CCC'],
     ['Twitter', '../images/icons/twitter-bird-dark-bgs.png', 'https://twitter.com/NARS_RRTraining', '#00ACED'],
     ['Facebook', '../images/icons/f_logo.png', 'http://www.facebook.com/pages/National-Academy-of-Railroad-Science/162421800480129', '#3B5B99'],
-    ['Pictures', '../images/icons/glossy-black-icon-people-things-camera.png', '#', '#0A3']
+    ['Pictures', '../images/icons/glossy-black-icon-people-things-camera.png', 'pictures.html', '#0A3']
   ],
 
   [
---
+++ js/pictures.js
@@ -0,0 +1,30 @@
+/* Pictures */
+
+function showPic(picId) {
+ if(document.getElementById("pic").style.display === "none")
+ {
+    /* Display Background Fill */
+    document.getElementById("picbg").style.display="block";
+    var con = document.getElementById("h"); /* div that contains of entire page */
+    var h = con.offsetHeight || con.clientHeight || con.scrollHeight; /* Try 3 ways to get the height. */
+
+    /* If the page height is greater than window height make the background fill the entire page */
+    if( h > window.innerHeight)
+    {
+     document.getElementById("picbg").style.height=(h + "px");
+    }
+
+    // Display Image
+    document.getElementById("pic").style.display="block";
+    document.getElementById("pic").style.backgroundImage=("url(" + document.getElementById(picId).getAttribute("src") + ")");
+    var pixelsScrolled=window.pageYOffset || document.documentElement.scrollTop || document.body.parentNode.scrollTop || document.body.scrollTop; /* Get distance scrolled by user */
+    document.getElementById("pic").style.top=(pixelsScrolled + "px"); /* Set the top equal to distance scrolled by user */
+
+ }
+ else
+ {
+    /* Hide background and image */
+    document.getElementById("picbg").style.display="none";
+    document.getElementById("pic").style.display="none";
+ }
+}
