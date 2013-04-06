677d35b3daf1a7a0ee9c67198ceeafbd92f4a998
studentofnars <studentofnars@gmail.com>
2013-04-05 19:57:26 -0500

    Fixed schedules

--- js/schedule.js
+++ js/schedule.js
@@ -3,14 +3,11 @@ function goToWeek(menu) {
 		// set week to selected value
 		var week = document.getElementById(menu.value);
 
-		// Reset menu value
-		menu.value='default';
-
 		// Scroll to week
 		scrollTo(0, week.offsetTop);
 
 		// Highlight the week temporarily
 		week.style.backgroundColor = '#00D'
-		setTimeout(function(){week.style.backgroundColor = ""}, 750);
+		setTimeout(function(){week.style.backgroundColor = ''}, 550);
 	}
 }
