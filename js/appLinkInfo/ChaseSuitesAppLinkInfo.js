c35e49dd2216567b7dc3bada5bd1137677b170fb
jason_gitaccount <forgitthisone@gmail.com>
2013-04-09 18:28:31 -0500

    Added cs cp hotels

---
+++ js/appLinkInfo/ChaseSuitesAppLinkInfo.js
@@ -0,0 +1,28 @@
+ChaseSuitesAppLinkInfo = new AppLinkLog(
+	[
+		['Back', '../images/icons/glossy-black-icon-arrows-arrow-styled-left.png', '../htdocs/hotels.html', '#CCC'],
+		['Chase Suites', '../images/icons/glossy-black-icon-business-home8.png', 'http://www.chasehoteloverlandpark.com', '#005B9B'],
+		['913-491-3333', '../images/icons/glossy-black-icon-business-phone2.png', 'tel:9134913333', '#A00'],
+		['Directions', '../images/icons/glossy-black-icon-business-globe.png', 'http://goo.gl/maps/qQgnJ', '#06F']
+	],
+
+	[
+		[],
+		[],
+		[],
+		[]
+	],
+
+	[
+		,
+		,
+		,
+		
+	]
+);
+
+function loadChaseSuitesAppLinks() {
+	loadAppLinks(ChaseSuitesAppLinkInfo, document.getElementById('appLinkAnchor'));
+	delete ChaseSuitesAppLinkInfo;
+	getSearchQuery();
+}
