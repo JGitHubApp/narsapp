b312c4b0c903fe65dadeda08ac51f0ab77397f49
jason_gitaccount <forgitthisone@gmail.com>
2013-04-11 05:19:21 -0500

    changed to fix slowness

--- js/pictures.js
+++ js/pictures.js
@@ -1,5 +1,19 @@
 /* Pictures */
 
+/* Monitor for orientation change */
+var previousOrientation = window.orientation;
+
+function updateBackground() {
+	/* used to update background fill */
+	if(document.getElementById("picbg").style.display === "block") {
+		var h = document.body.offsetHeight || document.body.scrollHeight || document.body.clientHeight; /* Try 3 ways to get the height. */
+
+		/* If the page height is greater than window height make the background fill the entire page */
+		if( h > window.innerHeight)
+			document.getElementById("picbg").style.height=(h + "px");
+	}
+}
+
 function showPic(pic) {
 	/* Display Background Fill */
 	document.getElementById("picbg").style.display="block";
@@ -23,17 +37,7 @@ function hidePic() {
 	document.getElementById("pclose").style.display="";
 }
 
-function updateBackground() {
-	/* used to update background fill */
-	if(document.getElementById("picbg").style.display === "block") {
-		var h = document.body.offsetHeight || document.body.clientHeight || document.body.scrollHeight; /* Try 3 ways to get the height. */
-
-		/* If the page height is greater than window height make the background fill the entire page */
-		if( h > window.innerHeight)
-			document.getElementById("picbg").style.height=(h + "px");
-	}
-}
-
+/* Monitor for orientation change */
 function checkOrientation() {
 	/* Check for orientation change */
 	if(window.orientation !== previousOrientation) {
@@ -42,6 +46,4 @@ function checkOrientation() {
 	}
 };
 
-/* Monitor for orientation change */
-var previousOrientation = window.orientation;
-window.addEventListener("resize", checkOrientation, false);
+window.onresize = checkOrientation;
