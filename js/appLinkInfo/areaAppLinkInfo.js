99070248b4d03825e554f5778e9810ff2d4fc4b7
studentofnars <studentofnars@gmail.com>
2013-04-17 18:39:41 -0500

    Added html page for JCCC map

--- js/appLinkInfo/areaAppLinkInfo.js
+++ js/appLinkInfo/areaAppLinkInfo.js
@@ -2,7 +2,7 @@ areaAppLinkInfo = new AppLinkLog(
 	[
 		['Back', '../images/icons/glossy-black-icon-arrows-arrow-styled-left.png', '../index.html', '#CCC'],
 		['Airport', '../images/icons/glossy-black-icon-transport-travel-transportation-airplane4.png', 'http://www.flykci.com', '#69E'],
-		['JCCC Campus Map', '../images/icons/glossy-black-icon-business-www-search-ps.png', '#', '#158'],
+		['JCCC Campus Map', '../images/icons/glossy-black-icon-business-www-search-ps.png', 'jcccMap.html', '#158'],
 		['5 Guys Transportation', '../images/icons/glossy-black-icon-transport-travel-transportation-car1.png', 'http://www.5guystransportation.com', '#00A'],
 		['Forecast', '../images/icons/glossy-black-icon-people-things-umbrella2.png', 'http://weather.yahoo.com', '#FC0']
 	],
