9dc3d5f58d5dc923e53aa634d3a39852f1f780b8
studentofnars <studentofnars@gmail.com>
2013-04-16 20:07:26 -0500

    Added new AppLinks

--- js/appLinkInfo/areaAppLinkInfo.js
+++ js/appLinkInfo/areaAppLinkInfo.js
@@ -3,6 +3,7 @@ areaAppLinkInfo = new AppLinkLog(
 		['Back', '../images/icons/glossy-black-icon-arrows-arrow-styled-left.png', '../index.html', '#CCC'],
 		['Airport', '../images/icons/glossy-black-icon-transport-travel-transportation-airplane4.png', 'http://www.flykci.com', '#69E'],
 		['JCCC Campus Map', '../images/icons/glossy-black-icon-business-www-search-ps.png', '#', '#158'],
+		['5 Guys Transportation', '../images/icons/glossy-black-icon-transport-travel-transportation-car1.png', 'http://www.5guystransportation.com', '#00A'],
 		['Forecast', '../images/icons/glossy-black-icon-people-things-umbrella2.png', 'http://weather.yahoo.com', '#FC0']
 	],
 
@@ -10,10 +11,12 @@ areaAppLinkInfo = new AppLinkLog(
 		[],
 		['plane', 'flew', 'directions', 'kansas', 'city', 'international'],
 		['johnson', 'county', 'community', 'college'],
+		['5', 'shuttle', 'service'],
 		['weather', 'report', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'tomorrow', 'weekly', 'day', 'today']
 	],
 
 	[
+		,
 		,
 		,
 		,
