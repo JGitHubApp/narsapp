cac30e7aa0959336e6a5412886ce2e48962b590a
studentofnars <studentofnars@gmail.com>
2013-04-22 21:56:14 -0500

    Made picture gallery more efficient

--- js/pictures.js
+++ js/pictures.js
@@ -3,13 +3,11 @@
 function resizePicture() {
 	var pic = document.getElementById('pic');
 
-	// Get window height and width
+	// Get window height
 	var winHeight = window.innerHeight || (document.documentElement.clientHeight + 15);
-	var winWidth = window.innerWidth || document.documentElement.clientWidth;
 	
 	// Contain the image within the window leaving a 15px offset
 	pic.style.maxHeight = (winHeight-30) + 'px';
-	pic.style.maxWidth = (winWidth-30) + 'px';
 
 	// Get pixels scrolled from top of browser
 	var pixelsScrolled=window.pageYOffset || document.documentElement.scrollTop || document.body.parentNode.scrollTop || document.body.scrollTop;
@@ -18,22 +16,25 @@ function resizePicture() {
 
 function showPic(aPic) {
 	document.getElementById('picbg').style.display = 'table';
-	pic = document.getElementById('pic').src = aPic.src.replace(/\/thumbnail-/, '/');
+	document.getElementById('pic').src = aPic.src.replace(/\/thumbnail-/, '/');
 
 	resizePicture();
-	var update = function() {
-		resizePicture();
+	update = resizePicture;
+	if (window.addEventListener) {
+		window.addEventListener('resize', update);
+		window.addEventListener('scroll', update);
 	}
-
-	window.addEventListener('resize', update, false);
-	window.addEventListener('scroll', update, false);
 }
 
 function hidePic() {
 	// Hide picbg
-	picbg = document.getElementById('picbg')
+	var picbg = document.getElementById('picbg');
 	picbg.style.display = '';
 	picbg.style.marginTop = '';
-	window.removeEventListener('resize', update, false);
-	window.removeEventListener('scroll', update, false);
+	document.getElementById('pic').src = '';
+
+	if (window.addEventListener) {
+		window.removeEventListener('resize', update);
+		window.removeEventListener('scroll', update);
+	}
 }
