acca425096933136fb7a4a06d72947f139879405
studentofnars <studentofnars@gmail.com>
2013-04-29 19:12:25 -0500

    Fixed schedule

--- js/appLinkInfo/classOneRailroadsAppLinkInfo.js
+++ js/appLinkInfo/classOneRailroadsAppLinkInfo.js
@@ -2,7 +2,7 @@ classOneRailroadsAppLinkInfo = new AppLinkLog(
 	[
 		['Back', '../images/icons/glossy-black-icon-arrows-arrow-styled-left.png', 'railroadIndustry.html', '#CCC'],
 		['BNSF', '../images/icons/glossy-black-icon-transport-travel-transportation-train8-sc43.png', 'http://www.bnsf.com', '#F85D13'],
-		['Canadian National', '../images/icons/glossy-black-icon-transport-travel-transportation-train8-sc43.png', 'http://www.cn.ca/en/careers.htm', '#242A32'],
+		['Canadian National', '../images/icons/glossy-black-icon-transport-travel-transportation-train8-sc43.png', 'http://www.cn.ca/en/careers.htm', '#335'],
 		['Canadian Pacific', '../images/icons/glossy-black-icon-transport-travel-transportation-train8-sc43.png', 'http://www.cpr.ca/en/Careers/Pages/default.aspx', '#C60B2F'],
 		['CSX Transportation', '../images/icons/glossy-black-icon-transport-travel-transportation-train8-sc43.png', 'http://www.csx.com', '#0E4980'],
 		['Kansas City Southern', '../images/icons/glossy-black-icon-transport-travel-transportation-train8-sc43.png', 'http://www.kcsouthern.com/en-us/Careers/Pages/CareersMain.aspx', '#C60B2F'],
