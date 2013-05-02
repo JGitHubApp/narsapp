55c19b450ed32c54e3f72d820e24b5331cb9bc2e
studentofnars <studentofnars@gmail.com>
2013-05-02 16:55:45 -0500

    Enabled left and right flash card flip directions

--- js/flashCardAnimations.js
+++ js/flashCardAnimations.js
@@ -1,12 +1,38 @@
-var cardIsFlipped = false;
+function getTransformProperty() {
+	var properties = [
+		'transform',
+		'WebkitTransform',
+		'msTransform',
+		'MozTransform',
+		'OTransform'
+	];
+	var p;
+	while (p = properties.shift()) {
+			if (typeof fc.style[p] != 'undefined') {
+					return p;
+			}
+	}
+	return false;
+}
 
-function flipFlashCard() {
-	if (cardIsFlipped)
-		fc.className = '';
+var rotateProperty = getTransformProperty();
+var cardIsFlipped = false;
+var degreesFlipped = 0;
+
+function flipCard() {
+	// Get x coordinate within card
+	var x = event.clientX - fc.getBoundingClientRect().left;
+
+	// If click was on Right side of card
+	if (x > fc.width / 2)
+		degreesFlipped += 180;
 	else
-		fc.className = 'flipped';
+		degreesFlipped -= 180;
+
+	// Flip card
+	fc.style[rotateProperty] = 'rotateY(' + degreesFlipped + 'deg)';
 
 	cardIsFlipped = !cardIsFlipped;
 }
 
-fc.addEventListener('click', flipFlashCard);
+fc.addEventListener('click', flipCard, false);
