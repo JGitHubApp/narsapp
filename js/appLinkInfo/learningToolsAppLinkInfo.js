133f41b7e2637a738ab3841d16972dcf35920ca9
studentofnars <studentofnars@gmail.com>
2013-05-11 17:20:57 -0500

    Removed flash card prototypes

--- js/appLinkInfo/learningToolsAppLinkInfo.js
+++ js/appLinkInfo/learningToolsAppLinkInfo.js
@@ -2,19 +2,14 @@ learningToolsAppLinkInfo = new AppLinkLog(
 	[
 		['Back', '../images/icons/glossy-black-icon-arrows-arrow-styled-left.png', '../index.html', '#CCC'],
 		['Signals Flash Cards', '../images/icons/glossy-black-icon-business-document1.png', 'flashCard.html', '#05A'],
-		['Flash Card Prototype', '../images/icons/glossy-black-icon-business-document1.png', 'flashCard2.html', '#FFF'],
-		['Flash Card Prototype 2', '../images/icons/glossy-black-icon-business-document1.png', 'flashCard3.html', '#FFF']
 	],
 
 	[
-		[],
-		[],
 		[],
 		[]
 	],
 
 	[
-		,
 		,
 
 	]
--- js/flashCardAnimations.js
+++ js/flashCardAnimations.js
@@ -12,7 +12,7 @@ else if ('MozTransform' in fc.style)
 // Check if flip effect is supported
 if (((LE === '' ? 'b':LE + 'B') + 'ackfaceVisibility') in fc.style && !!(window.SVGSVGElement)) {
 	// Use high degree for browsers that don't support negative rotation
-	var degreesFlipped = 3600000;
+	var degreesFlipped = 3600000.01;
 
 	// Get transform type for layout engine
 	var transformType = ((LE === '' ? 't':LE + 'T') + 'ransform')
@@ -32,7 +32,7 @@ if (((LE === '' ? 'b':LE + 'B') + 'ackfaceVisibility') in fc.style && !!(window.
 	}, 100);
 
 	function updateRotation(degrees) {
-		fc.style[transformType] = fc.style[transformType].replace(/(?!rotateY\()\d+/, degrees);
+		fc.style[transformType] = fc.style[transformType].replace(/(?!rotateY\()\d+(\.\d+)?/, degrees);
 	}
 
 	// Flip flash card over
@@ -44,6 +44,25 @@ if (((LE === '' ? 'b':LE + 'B') + 'ackfaceVisibility') in fc.style && !!(window.
 		
 		updateRotation(degreesFlipped);
 		cardIsFlipped = !cardIsFlipped;
+	}
+
+	animateCard = function(direction) {
+		switch (direction) {
+			case 'left':
+			case 'right':
+				flipCard(direction);
+				break;
+			case 'up':
+				fc.classList.add('moveUp');
+				var t1 = setTimeout(goNextCard, 250);
+				var t2 = setTimeout(function() {fc.classList.remove('moveUp');}, 500);
+				break;
+			case 'down':
+				fc.classList.add('moveDown');
+				var t1 = setTimeout(goBackCard, 250);
+				var t2 = setTimeout(function() {fc.classList.remove('moveDown');}, 500);
+				break;
+		}
 	}
 }
 else { // If flip effect is not supported
@@ -60,6 +79,21 @@ else { // If flip effect is not supported
 		}
 
 		cardIsFlipped = !cardIsFlipped;
+	}
+
+	animateCard = function(direction) {
+		switch (direction) {
+			case 'left':
+			case 'right':
+				flipCard(direction);
+				break;
+			case 'up':
+				goNextCard();
+				break;
+			case 'down':
+				goBackCard();
+				break;
+		}
 	}
 }
 
@@ -82,7 +116,125 @@ function goNextCard() {
 
 	drawSignal(signalArray[currentRule][currentSignal], ruleArray[currentRule]);
 }
+window.addEventListener('keydown', function(event) {
+		switch (event.which) {
+			case 37: // LEFT
+				animateCard('left');
+				break;
+			case 39: // RIGHT
+				animateCard('right');
+				break;
+			case 38: // UP
+				animateCard('up');
+				break;
+			case 40: // DOWN
+				animateCard('down');
+				break;
+		}
+	}, false);
 
-fc.addEventListener('click', flipCard, false);
-document.getElementById('gotoBackCard').addEventListener('click', goBackCard, false);
-document.getElementById('gotoNextCard').addEventListener('click', goNextCard, false);
+// If touch events are supported
+if ('ontouchstart' in window) {
+	var touchX = null;
+	var touchY = null;
+	var tiltDirection = null;
+	var prevTransform = null;
+
+	window.addEventListener('touchstart', function() {
+		touchX = event.touches[0].pageX;
+		touchY = event.touches[0].pageY;
+
+		event.preventDefault();
+		prevTransform = fc.style[transformType];
+	});
+
+	window.addEventListener('touchend', function() {
+		if (touchX != null && touchY != null) {
+			fc.style[transformType] = prevTransform;
+			prevTransform = null;
+
+			var endX = event.changedTouches[0].pageX;
+			var endY = event.changedTouches[0].pageY;
+			var bb = document.getElementById('backButton').getElementsByTagName('img')[0];
+
+			// If swipe length is long enough
+			//if (Math.sqrt(Math.pow(touchX - endX, 2) + Math.pow(touchY - endY, 2)) > 50) {
+			if (Math.abs(endX - touchX) > 50 || Math.abs(endY - touchY) > 50) {
+				event.preventDefault();
+				animateCard(swipeDirection(endX, endY));
+			}
+			else if ( // On return button
+					touchX >= bb.offsetLeft &&
+					touchX <= bb.offsetLeft + bb.offsetWidth &&
+					touchY >= bb.offsetTop &&
+					touchY <= bb.offsetTop + bb.offsetHeight) {
+
+				location.href = document.getElementById('backButton').href;
+			}
+		}
+
+		resetTouchEvent();
+	});
+
+	window.addEventListener('touchmove', function() {
+		if (touchX != null && touchY != null) {
+			event.preventDefault();
+
+			var curX = event.touches[0].pageX;
+			var curY = event.touches[0].pageY;
+
+			// If tilt direction has changed
+			if (tiltDirection != swipeDirection(curX, curY)) {
+				// Reset Tilt
+				fc.style[transformType] = prevTransform;
+				distX = Math.abs(curX - touchX);
+
+				// If length of swipe is long enough
+				if (distX > 50) {
+					tiltDirection = swipeDirection(curX, curY);
+					var tiltAmount = 15;
+
+					switch (tiltDirection) {
+						case 'left':
+							tiltAmount = degreesFlipped - tiltAmount;
+							updateRotation(tiltAmount);
+							break;
+						case 'right':
+							tiltAmount = degreesFlipped + tiltAmount;
+							updateRotation(tiltAmount);
+							break;
+					}
+				}
+			}
+		}
+	});
+
+	function resetTouchEvent() {
+		touchX = null;
+		touchY = null;
+		tiltDirection = null;
+		if (prevTransform)
+			fc.style[transformType] = prevTransform;
+		prevTransform = null;
+	}
+
+	function swipeDirection(endX, endY) {
+		var strDirection = '';
+
+		if (Math.abs(touchX - endX) > Math.abs(touchY - endY))
+			if (touchX > endX)
+				strDirection = 'left';
+			else
+				strDirection = 'right';
+		else
+			if (touchY > endY)
+				strDirection = 'up';
+			else
+				strDirection = 'down';
+
+		return strDirection;
+	}
+}
+else {
+	fc.addEventListener('click', flipCard, false);
+}
